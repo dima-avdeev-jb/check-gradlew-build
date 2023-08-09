1919da67da0053b4e14238c92854a36ab6deb112
dima.avdeev <dima.avdeev@jetbrains.com>
2023-08-09 15:40:13 +0400

    minify

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,8 +1,6 @@
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     Text("Hello")
